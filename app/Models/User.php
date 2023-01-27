55df76e03f732a32988bbd8ead180c464147154f
Juan Esteban <juanes0202@gmail.com>
2023-01-27 15:11:43 -0500

    instalanado sanctum para gestionar la autenticacion API

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,17 +1,19 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use HasApiTokens, Notifiable;
 
     /**
      * The attributes that are mass assignable.
+     *
      *
      * @var array
      */
